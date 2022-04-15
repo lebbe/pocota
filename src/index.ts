d129eaab91070d5731e6756c01fed7916096f8af
Lars-Erik Bruce <lebchen@gmail.com>
2022-04-15 16:35:54 +0200

    More compact index file.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
-import { Table as TableImport } from './components/Table'
-import { Tbody as TbodyImport } from './components/Tbody'
-import { Td as TdImport } from './components/Td'
-import { Th as ThImport } from './components/Th'
-import { Thead as TheadImport } from './components/Thead'
-import { Tr as TrImport } from './components/Tr'
-import { Tfoot as TfootImport } from './components/Tfoot'
-
-export const Table = TableImport
-export const Th = ThImport
-export const Thead = TheadImport
-export const Td = TdImport
-export const Tr = TrImport
-export const Tbody = TbodyImport
-export const Tfoot = TfootImport
+export { Table } from './components/Table'
+export { Tbody } from './components/Tbody'
+export { Td } from './components/Td'
+export { Th } from './components/Th'
+export { Thead } from './components/Thead'
+export { Tr } from './components/Tr'
+export { Tfoot } from './components/Tfoot'
